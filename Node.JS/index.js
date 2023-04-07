6382e74704ce9a1363a480cd8e8b6bcb61feea45
Augusto Rodrigues <augustorodrigues14981@gmail.com>
2023-04-07 15:02:01 -0300

    parametros

--- Node.JS/index.js
+++ Node.JS/index.js
@@ -13,6 +13,10 @@ app.get('/blog', function(req, res){
     res.send('Esta e a minha pagina "blog"')
 })
 
+app.get('/ola/:nome/:cargo', function(req, res){
+    res.send('<h1>Ola ' + req.params.nome + '</h1>' + '<h2> Seu cargo e: ' + req.params.cargo + '</h2>')
+})
+
 app.listen(8081, function(){
     console.log('Servidor rodando na URL http://localhost:8081')
-})//deve ser a última linha do códico
+})//deve ser a última linha do código
