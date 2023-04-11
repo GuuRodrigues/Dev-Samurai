b1b5b27120f403b7d7553698a0a2a7a118b1a815
Augusto Rodrigues <augustorodrigues14981@gmail.com>
2023-04-11 19:24:33 -0300

    exibindo HTML com node.js

--- Node.JS/index.js
+++ Node.JS/index.js
@@ -2,18 +2,18 @@ const express = require('express')
 const app = express()
 
 app.get('/', function(req, res){
-    res.send('Seja bem-vendo ao meu website!')
+    res.sendFile(__dirname + "/html/index.html")
 })
 
 app.get('/sobre', function(req, res){
-    res.send('Esta e a minha pagina "sobre"')
+    res.sendFile(__dirname + "/html/sobre.html")
 })
 
 app.get('/blog', function(req, res){
-    res.send('Esta e a minha pagina "blog"')
+    res.sendFile(__dirname + "/html/blog.html")
 })
 
-app.get('/ola/:nome/:cargo', function(req, res){
+app.get('/ola/:nome/:cargo', function(req, res){//parametros
     res.send('<h1>Ola ' + req.params.nome + '</h1>' + '<h2> Seu cargo e: ' + req.params.cargo + '</h2>')
 })
 
