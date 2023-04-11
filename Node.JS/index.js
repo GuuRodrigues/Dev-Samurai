8987d8593a99b868dfac0990f74aa5fa3ce267bf
Augusto Rodrigues <augustorodrigues14981@gmail.com>
2023-04-10 21:52:17 -0300

    nodemon

--- Node.JS/index.js
+++ Node.JS/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function(req, res){
-    res.send('Seja bem-vendo ao meu app!')
+    res.send('Seja bem-vendo ao meu website!')
 })
 
 app.get('/sobre', function(req, res){
